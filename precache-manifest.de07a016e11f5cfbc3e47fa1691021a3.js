b614b4a54fb900e4ada26cb0caafc316f0e84953
Ken Huang <kenhuang.au@gmail.com>
2019-07-21 00:27:26 +1000

    Updates

---
+++ precache-manifest.de07a016e11f5cfbc3e47fa1691021a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05adc6d0a44085a9e146d7003744d49b",
+    "url": "/horse-racing-odds/index.html"
+  },
+  {
+    "revision": "e7900a7d30485bfa198a",
+    "url": "/horse-racing-odds/static/css/2.e6157a56.chunk.css"
+  },
+  {
+    "revision": "2132bac159f1526d19bd",
+    "url": "/horse-racing-odds/static/css/main.4e8d29a3.chunk.css"
+  },
+  {
+    "revision": "e7900a7d30485bfa198a",
+    "url": "/horse-racing-odds/static/js/2.f6ac806e.chunk.js"
+  },
+  {
+    "revision": "2132bac159f1526d19bd",
+    "url": "/horse-racing-odds/static/js/main.b21a583d.chunk.js"
+  },
+  {
+    "revision": "d5e3936e5db178ccb8d4",
+    "url": "/horse-racing-odds/static/js/runtime~main.e635c925.js"
+  }
+]);
