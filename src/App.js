03acdd531d6ce6b84e439bac798326e9f229911a
Ken Huang <kenhuang.au@gmail.com>
2019-07-21 13:06:16 +1000

    Add header for the whole app

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import DocumentTitle from 'react-document-title'
 import Index from './pages/index/'
 
 export default function App () {
-  // should use router if there are more pages
+  const title = 'Horse Racing Odds'
   return (
-    <DocumentTitle title='Horse Racing Odds'>
-      <main className='container' style={{ padding: '1.5rem .75rem' }}>
-        <Index />
-      </main>
+    <DocumentTitle title={title}>
+      <div>
+        <header className='hero is-info is-small'>
+          <div className='hero-body'>
+            <div className='container'>
+              <h1 className='title has-text-centered'>
+                { title }
+              </h1>
+            </div>
+          </div>
+        </header>
+        <main className='container' style={{ padding: '1.5rem .75rem' }}>
+          {/* should use router if there are more pages */}
+          <Index />
+        </main>
+      </div>
     </DocumentTitle>
   )
 }
